c745766888b4db48adcb68f29a977cbbc4915c6b
juan-alvarz <juanalvarez0925@gmail.com>
2023-03-18 21:24:59 -0500

    Initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home Test</title>
+      </Head>
+      <main>
+        <h1>Main Page Application</h1>
+        <div>
+          <p>This is the test for connect with api</p>
+        </div>
+      </main>
+    </>
+  );
+}
